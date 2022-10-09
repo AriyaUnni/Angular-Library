bcd70c6fee6158ac408fe2900f096ba88a8df3f6
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-09 22:41:30 +0530

    home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,13 @@ import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ContactComponent } from './contact/contact.component';
+import { HomeComponent} from './home/HomeComponent.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes:Routes=[
+  {
+    path:"",component:HomeComponent
+  },
   {
     path:"about",component:AboutComponent
   },
