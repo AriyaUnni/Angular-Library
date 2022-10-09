68f3fde01e7ab2d2b5b556e0255c00fdc3342cc7
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-09 23:11:22 +0530

    devep page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ContactComponent } from './contact/contact.component';
-import { HomeComponent } from './home/home.component';
+
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
